68dcdee45dab42e56007f56938b184c3a8e331a3
Revek310 <kocholi8775@gmail.com>
2024-11-06 13:44:54 +0100

    PEB-10 PEB-51 PEB-48 PEB-56 Integrationstests für Projekt. Endpunkte: GET, POST, PUT, DELETE

---
+++ src/test/java/de/szut/lf8_starter/project/ProjectGetEmployeeByProject.java
@@ -0,0 +1,45 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+
+public class ProjectGetEmployeeByProject extends AbstractIntegrationTest {
+
+    private int testProjectId = 1;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Util.createProjectTestData(projectRepository, testProjectId);
+    }
+
+    @Test
+    void authorization() throws Exception {
+        this.mockMvc.perform(get("/projects/finAllEmployeesByProject/{id}" + testProjectId)
+                        .with(csrf()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(roles = "user")
+    void getProjectById() throws Exception {
+
+        this.mockMvc.perform(get("/projects/finAllEmployeesByProject/{id}", testProjectId)
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+
+
+
+
+
+}
