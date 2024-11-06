bcf00d8230249b4f30e214f792ef070fe9d996e4
„Shawkat <shawkat1@schule.bremen.de>
2024-11-06 12:59:46 +0100

    update

--- src/main/java/de/szut/lf8_starter/employee/EmployeeService.java
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeService.java
@@ -60,17 +60,5 @@ public class EmployeeService {
             throw new RuntimeException("Fehler beim Abrufen des Mitarbeiters: " + e.getMessage(), e);
         }
     }
-
-
-//    private String getJwtToken() {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if (auth instanceof JwtAuthenticationToken) {
-//            JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) auth;
-//            Jwt jwt = jwtAuthenticationToken.getToken();
-//            String tokenValue = jwt.getTokenValue();
-//            return "Bearer " + tokenValue;
-//        }
-//        return "JWT Token Not found";
-//    }
 }
 
