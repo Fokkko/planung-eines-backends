f3cc5b32cd09b73bd578411cfb9576e2c053bb6a
„Shawkat <shawkat1@schule.bremen.de>
2024-11-06 23:44:22 +0100

    refactored👌👌👌

--- src/main/java/de/szut/lf8_starter/project/ProjectController.java
+++ src/main/java/de/szut/lf8_starter/project/ProjectController.java
@@ -10,9 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
